0a998a6970c3fa29139bf6245756728a1232353d
Ivan Diordiev <74953631+idiordiev@users.noreply.github.com>
2022-10-05 18:53:12 +0300

    added class for paging options

---
+++ Application/Utility/PagingParameters.cs
@@ -0,0 +1,34 @@
+﻿namespace eStore_Admin.Application.Utility
+{
+    public class PagingParameters
+    {
+        private const int MaxPageSize = 100;
+        
+        private int _pageNumber;
+        private int _pageSize;
+
+        public int PageNumber
+        {
+            get => _pageNumber;
+            set => _pageNumber = value > 0 ? value : 1;
+        }
+
+        public int PageSize
+        {
+            get => _pageSize;
+            set => _pageSize = value < MaxPageSize ? value : MaxPageSize;
+        }
+
+        public PagingParameters()
+        {
+            PageNumber = 1;
+            PageSize = 10;
+        }
+
+        public PagingParameters(int pageSize, int pageNumber)
+        {
+            PageSize = pageSize;
+            PageNumber = pageNumber;
+        }
+    }
+}
