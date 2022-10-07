8df475dfe392ac75a04a407a83394199a12e067c
Ivan Diordiev <74953631+idiordiev@users.noreply.github.com>
2022-10-07 11:48:53 +0300

    added dependencies to Startup

--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -1,18 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using eStore_Admin.Application.Extensions;
+using eStore_Admin.Infrastructure.Extensions;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 
-namespace WebApi
+namespace eStore_Admin.WebApi
 {
     public class Startup
     {
@@ -27,6 +22,10 @@ namespace WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            
+            services.AddApplicationLayer();
+            services.AddInfrastructureLayer(Configuration);
+
             services.AddSwaggerGen(c => { c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebApi", Version = "v1" }); });
         }
 
