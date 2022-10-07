40a6f9a2bf7c8a1bc539aa59bf0d6347d3667a0c
Ivan Diordiev <74953631+idiordiev@users.noreply.github.com>
2022-10-07 16:28:00 +0300

    minor fix

--- Application/Requests/Customers/Queries/GetByFilterPaged/GetCustomerByFilterPagedQuery.cs
+++ Application/Requests/Customers/Queries/GetByFilterPaged/GetCustomerByFilterPagedQuery.cs
@@ -10,15 +10,5 @@ namespace eStore_Admin.Application.Requests.Customers.Queries.GetByFilterPaged
     {
         public PagingParameters PagingParameters { get; set; }
         public CustomerFilterModel FilterModel { get; set; }
-
-        // public ICollection<bool?> IsDeletedValues { get; set; }
-        // public string FirstName { get; set; }
-        // public string LastName { get; set; }
-        // public string Email { get; set; }
-        // public string PhoneNumber { get; set; }
-        // public string Country { get; set; }
-        // public string City { get; set; }
-        // public string Address { get; set; }
-        // public string PostalCode { get; set; }
     }
 }
